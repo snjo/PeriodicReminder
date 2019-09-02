8a11b09beed94c13d5bc1c554e0682c67d8b2dd2
Andreas Aakvik Gogstad <agogstad@gmail.com>
2019-09-02 13:09:55 +0200

    Initial commit

---
+++ PeriodicReminder/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace PeriodicReminder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PerformedTask = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.StartTimerButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PerformedTask
+            // 
+            this.PerformedTask.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PerformedTask.Location = new System.Drawing.Point(12, 73);
+            this.PerformedTask.Name = "PerformedTask";
+            this.PerformedTask.Size = new System.Drawing.Size(260, 161);
+            this.PerformedTask.TabIndex = 0;
+            this.PerformedTask.Text = "I did the thing";
+            this.PerformedTask.UseVisualStyleBackColor = true;
+            this.PerformedTask.Click += new System.EventHandler(this.PerformedTask_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 12);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(57, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(13, 39);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(56, 20);
+            this.numericUpDown2.TabIndex = 2;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Minutes to first alert";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Minutes between future alerts";
+            // 
+            // StartTimerButton
+            // 
+            this.StartTimerButton.Location = new System.Drawing.Point(227, 8);
+            this.StartTimerButton.Name = "StartTimerButton";
+            this.StartTimerButton.Size = new System.Drawing.Size(45, 51);
+            this.StartTimerButton.TabIndex = 5;
+            this.StartTimerButton.Text = "Start";
+            this.StartTimerButton.UseVisualStyleBackColor = true;
+            this.StartTimerButton.Click += new System.EventHandler(this.StartTimer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 246);
+            this.Controls.Add(this.StartTimerButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.PerformedTask);
+            this.Name = "Form1";
+            this.Text = "Periodic Reminder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PerformedTask;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button StartTimerButton;
+    }
+}
+
---
+++ PeriodicReminder/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace PeriodicReminder
+{
+    public partial class Form1 : Form
+    {
+        System.Timers.Timer timer;
+        //System.Timers.Timer debugTimer;
+        //float minutes = 56f;
+        //float resetMinutes = 60f;
+
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //StartTimer();
+
+            //debugTimer = new System.Timers.Timer(minutes * 60000);
+            //debugTimer.Elapsed += DebugTimerEvent;
+            //debugTimer.AutoReset = true;
+            //debugTimer.Enabled = true;
+        }
+
+        private void StartTimer()
+        {
+            timer = new System.Timers.Timer((Double)numericUpDown1.Value * 60000d);
+            timer.Elapsed += ElapsedTimer;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+
+        private void DebugTimerEvent(object sender, ElapsedEventArgs e)
+        {
+            //label1.Text = "test";
+        }
+
+        private void ElapsedTimer(object sender, ElapsedEventArgs e)
+        {
+            PerformedTask.BackColor = Color.Red;
+            timer.Interval = (Double)numericUpDown2.Value *60000d;
+            timer.Enabled = true;
+        }
+
+        private void PerformedTask_Click(object sender, EventArgs e)
+        {
+            PerformedTask.BackColor = Color.LightGray;
+        }
+
+        /*private void label1_Click(object sender, EventArgs e)
+        {
+
+        }*/
+
+        private void StartTimer_Click(object sender, EventArgs e)
+        {
+            StartTimer();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
